b84f66be195dc5bc683ca4a6fa2fbed260f2d806
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-04 11:50:08 -0600

    :bookmark: comments - blade

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,23 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    @for ($i = 0; $i < 10; $i++)
-        <p>index value is {{ $i }}</p>
-    @endfor
-    @foreach($numbers as $number)
-        <p>N: {{$number}}</p>
-    @endforeach
-
-    @while($x < 10)
-        <p>{{$x++}}</p>
-    @endwhile
-
-    @foreach ($users as $user)
-        <div class="card">
-            <div class="card-title"></div>
-            <div class="card-body">
-                {{$user->name}}
-            </div>
-        </div>
-    @endforeach
+    <h1>Comments</h1>
+    {{ $x }}
+    {{-- $x --}}
 @endsection
